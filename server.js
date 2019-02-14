b04fbcb0ce2a302d14433a96b07ba45cd04da8b4
Halal-Face <Zahin.Mohammad@gmail.com>
2019-02-14 17:40:18 -0500

    scopes

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.get('/login', function(req, res) {
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      scope: 'streaming user-read-birthdate user-read-email user-read-private',
       redirect_uri
     }))
-})
+});
 
 app.get('/callback', function(req, res) {
   let code = req.query.code || null
@@ -28,18 +28,52 @@ app.get('/callback', function(req, res) {
       grant_type: 'authorization_code'
     },
     headers: {
-      'Authorization': 'Basic ' + (new Buffer(
+      'Authorization': 'Basic ' + (new Buffer.from(
         process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
       ).toString('base64'))
     },
     json: true
   }
   request.post(authOptions, function(error, response, body) {
+    if (error){
+      console.log(error)
+    }
     var access_token = body.access_token
+    var refresh_token = body.refresh_token
+    // console.log(body)
     let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
-    res.redirect(uri + '?access_token=' + access_token)
+    res.redirect(`${uri}?access_token=${access_token}&refresh_token=${refresh_token}`)
   })
-})
+});
+
+app.get('/refresh_token', function(req, res) {
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+  console.log("Sending Request")
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
 
 let port = process.env.PORT || 8888
 console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
